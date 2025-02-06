7d324a8208a33379f67c051410836194289a59c3
kingwill101 <hey@glenfordwilliams.com>
2025-02-05 19:39:45 -0500

    routed: improve cookie handling

--- packages/routed/lib/middlewares.dart
+++ packages/routed/lib/middlewares.dart
@@ -1,5 +1,4 @@
 export 'src/middleware/timeout.dart';
 export 'src/middleware/basic_auth.dart';
-export 'src/middleware/session.dart';
 export 'src/middleware/request_tracker.dart';
 export 'src/router/types.dart' show Middleware;
