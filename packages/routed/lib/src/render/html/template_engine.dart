2ab1a1eb918f2981aaf1e9c8806331a0c2207dda
kingwill101 <hey@glenfordwilliams.com>
2025-03-19 10:14:38 -0500

    refactor: remove Jinja template engine and enhance Liquid template engine with function and filter registration

--- packages/routed/lib/src/render/html/template_engine.dart
+++ packages/routed/lib/src/render/html/template_engine.dart
@@ -4,11 +4,22 @@ abstract class TemplateEngine {
   /// [templateName] is the name of the template to be rendered.
   /// [data] is a map containing key-value pairs that will be used to populate the template.
   /// Returns a [Future] that completes with the rendered template as a [String].
-  Future<String> render(String templateName, Map<String, dynamic> data);
+  Future<String> render(String templateName,
+      [Map<String, dynamic> data = const {}]);
+
+  String renderContent(String content, [Map<String, dynamic> data = const {}]);
 
   /// Loads templates from the specified path.
   ///
   /// [path] is the directory path where the templates are located.
   /// This method should be called before attempting to render any templates.
   void loadTemplates(String path);
+
+  Map<String, Function> get funcMap;
+
+  Map<String, Function> get filterMap;
+
+  void addFunc(String name, Function fn);
+
+  void addFilter(String name, Function filter);
 }
