35cf6e795bfb07a8017ffaa0047e93bccabfd38b
kingwill101 <hey@glenfordwilliams.com>
2026-03-04 06:17:58 -0500

    refactor(routed_openapi): move route metadata merger

--- packages/routed_openapi/lib/routed_openapi.dart
+++ packages/routed_openapi/lib/routed_openapi.dart
@@ -6,5 +6,6 @@ export 'src/openapi/manifest_to_openapi.dart';
 export 'src/openapi/openapi_spec.dart';
 export 'src/openapi/pipe_rule_converter.dart';
 export 'src/openapi/route_metadata_extractor.dart';
+export 'src/openapi/route_metadata_merger.dart';
 export 'src/openapi/route_manifest.dart';
 export 'src/openapi/schema.dart';
