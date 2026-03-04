e15bf130debf85585d73709a9f38a5fe15bcf2a1
kingwill101 <hey@glenfordwilliams.com>
2026-03-04 03:57:16 -0500

    refactor(routed_core): move driver registry primitives

--- packages/routed_core/lib/routed_core.dart
+++ packages/routed_core/lib/routed_core.dart
@@ -6,6 +6,7 @@ export 'src/events/event.dart';
 export 'src/events/event_manager.dart';
 export 'src/provider/provider.dart';
 export 'src/provider/config_utils.dart';
+export 'src/support/driver_registry.dart';
 export 'src/support/named_registry.dart';
 export 'src/utils/deep_copy.dart';
 export 'src/utils/dot.dart';
