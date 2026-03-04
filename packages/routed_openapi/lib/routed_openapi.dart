95f4286b3a66fd74d1a3c708001dea0d366f05e4
kingwill101 <hey@glenfordwilliams.com>
2026-03-04 06:15:09 -0500

    refactor(routed_openapi): move manifest to openapi converter

--- packages/routed_openapi/lib/routed_openapi.dart
+++ packages/routed_openapi/lib/routed_openapi.dart
@@ -2,6 +2,7 @@ library;
 
 export 'src/openapi/annotations.dart';
 export 'src/openapi/handler_identity.dart';
+export 'src/openapi/manifest_to_openapi.dart';
 export 'src/openapi/openapi_spec.dart';
 export 'src/openapi/pipe_rule_converter.dart';
 export 'src/openapi/route_metadata_extractor.dart';
