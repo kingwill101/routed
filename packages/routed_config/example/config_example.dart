d1a20a950b18ac42490dc9d0642316bd0d34f830
kingwill101 <hey@glenfordwilliams.com>
2026-08-10 02:57:21 -0500

    chore: remove routed_config facade package
    Config lives in routed_core; the separate package only re-exported it.

--- packages/routed_config/example/config_example.dart
+++
@@ -1,5 +0,0 @@
-import 'package:routed_config/routed_config.dart';
-
-void main() {
-  print('routed_config example: facade re-exporting routed config');
-}
--- packages/routed_config/lib/routed_config.dart
+++
@@ -1,8 +0,0 @@
-library;
-export 'package:routed_core/src/config/config.dart';
-export 'package:routed_core/src/config/helpers.dart';
-export 'package:routed_core/src/config/loader.dart';
-export 'package:routed_core/src/config/registry.dart';
-export 'package:routed_core/src/config/runtime.dart';
-export 'package:routed_core/src/config/schema.dart';
-export 'src/specs.dart';
