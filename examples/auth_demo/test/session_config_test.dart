2e4d348fdc74209dce3cff7b4be7baa8275a99f9
kingwill101 <hey@glenfordwilliams.com>
2026-09-14 10:24:17 -0500

    test(auth): cover jwt demo startup

--- examples/auth_demo/test/session_config_test.dart
+++ examples/auth_demo/test/session_config_test.dart
@@ -1,4 +1,5 @@
 import 'package:auth_demo/app.dart' show createEngine;
+import 'package:auth_demo/jwt_app.dart' show createJwtEngine;
 import 'package:routed/routed.dart';
 import 'package:server_auth_ormed/server_auth_ormed.dart';
 import 'package:test/test.dart';
@@ -11,6 +12,13 @@ void main() {
     final config = engine.container.get<SessionConfig>();
     expect(config.secure, isFalse);
     expect(config.defaultOptions.secure, isFalse);
+    expect(engine.container.get<AuthOptions>().store, isA<OrmAuthStore>());
+  });
+
+  test('JWT demo starts with the Ormed auth store', () async {
+    final engine = await createJwtEngine();
+    addTearDown(engine.close);
+
     expect(engine.container.get<AuthOptions>().store, isA<OrmAuthStore>());
   });
 }
